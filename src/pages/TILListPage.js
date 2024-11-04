8fd766d790bc1c1df0a649e4b7ae0b2ab3c0797a
신종신 <jongsin16@naver.com>
2024-11-04 19:59:37 +0900

    feat: search 초기 구현 완료

--- src/pages/TILListPage.js
+++ src/pages/TILListPage.js
@@ -8,14 +8,19 @@ const TILListPage = () => {
     const [page, setPage] = useState(0);
     const [size, setSize] = useState(10);
     const [totalPage, setTotalPage] = useState();
-    const [hasMore, setHasMore] = useState(true);
+    const [keyword, setKeyword] = useState();
     const navigate = useNavigate();
 
     useEffect(() => {
-        const fetchPosts = async (page, size) => {
+        const fetchPosts = async (page, size, keyword) => {
             const params = {page, size};
+
             try{
-                const response = await axiosInstance.get('/v1/api/til',{params})
+                if(keyword){
+                    params.keyword = keyword;
+                }
+
+                const response = await axiosInstance.get(keyword ? 'v1/api/til/search' : '/v1/api/til', {params} )
                 console.log(response);
                 setPosts(response.data.data.list);
                 setTotalPage(response.data.data.totalPage);
@@ -24,8 +29,8 @@ const TILListPage = () => {
             }
         }
 
-        fetchPosts(page, size);
-    }, [page, size]); 
+        fetchPosts(page, size, keyword);
+    }, [page, size, keyword]); 
 
     const handleWrite = () => {
 		navigate('/TILSavePage');
@@ -43,9 +48,30 @@ const TILListPage = () => {
         }
     };
 
+    const handleSearchSubmit = (e) => {
+        if(e.key === "Enter"){
+            if(e.target.value.length < 2){
+                alert("두글자 이상을 입력해주세요");
+                setKeyword(null);
+            } else {
+                setKeyword(e.target.value); // Update keyword as the user types
+                setPage(0); // Reset to the first page on new search
+            } 
+        }
+    };
+
     return (
         <div style={{ padding: '20px' }}>
         <h1>Posts</h1>
+
+        <input 
+            type="text" 
+            placeholder="Search posts..." 
+            //onChange={(e) => setKeyword(e.target.value)}
+            onKeyDown={(e) => handleSearchSubmit(e)} // Trigger search on Enter
+            style={{ padding: '8px', marginBottom: '20px', width: '100%' }}
+        />
+
         <div>
             {posts && posts.map((post) => (
             <div key={post.id} style={{ border: '1px solid #ddd', padding: '10px', marginBottom: '10px' }}>
