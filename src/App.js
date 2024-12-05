46e5200e39c1bac1b018b1af4467ca40012d93db
신종신 <jongsin16@naver.com>
2024-12-05 14:03:29 +0900

    refactor: convert fcm to email

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, {useEffect} from 'react';
 import {Route, Routes} from 'react-router-dom';
-import {onMessage} from 'firebase/messaging';
-import {messaging} from './firebase-config'; // 경로 수정
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
@@ -33,25 +31,6 @@ function App() {
 }
 
 function MemberLayout(){
-  useEffect(() => {
-    // 메시지 수신 리스너 설정
-    const unsubscribe = onMessage(messaging, (payload) => {
-      console.log('Message received: ', payload);
-      if (payload.notification) {
-        const notificationTitle = payload.notification.title;
-        const notificationOptions = {
-          body: payload.notification.body,
-          icon: '/firebase-logo.png',
-        };
-
-        // 포그라운드에서 알림 표시
-        new Notification(notificationTitle, notificationOptions);
-      }
-    });
-
-    // 컴포넌트 언마운트 시 리스너 해제
-    return () => unsubscribe();
-  }, []);
 
   return (
     <div>
